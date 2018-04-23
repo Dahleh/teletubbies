4d4ab624b9491ff8e7a291a025dcf27d621e9db6
Faris Dahleh <farisdahleh@Live.com>
2018-04-24 02:32:59 +0300

    Login VC

---
+++ teletubbies/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  teletubbies
+//
+//  Created by Faris Dahleh on 4/24/18.
+//  Copyright © 2018 Faris Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ teletubbies/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  teletubbies
+//
+//  Created by Faris Dahleh on 4/24/18.
+//  Copyright © 2018 Faris Dahleh. All rights reserved.
+//
+
+import Foundation
+
+
+let TO_LOGIN = "toLogin"
