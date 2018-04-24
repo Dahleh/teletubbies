1cfc89c9786d581b6aa81183c20b5431c9bd4b24
Faris Dahleh <farisdahleh@Live.com>
2018-04-24 18:33:40 +0300

    Register User

--- teletubbies/Controller/CreateAccountVC.swift
+++ teletubbies/Controller/CreateAccountVC.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    //Outlets
+    
+    @IBOutlet var usernameTxt: UITextField!
+    @IBOutlet var emailTxt: UITextField!
+    @IBOutlet var passwordTxt: UITextField!
+    @IBOutlet var userImg: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else {return}
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success{
+                print("registered user!")
+            }
+        }
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
