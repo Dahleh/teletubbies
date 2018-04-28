837eb2b999933305d85171e121cd7093d3315006
Faris Dahleh <farisdahleh@Live.com>
2018-04-29 02:38:24 +0300

    Fetching Messages

--- teletubbies/Controller/ChatVC.swift
+++ teletubbies/Controller/ChatVC.swift
@@ -18,19 +18,18 @@ class ChatVC: UIViewController {
         super.viewDidLoad()
         
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
+        
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail { (success) in
                 NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
             }
         }
-        
-
-        
     }
     
     @objc func userDataDidChanged(_ notif: Notification){
@@ -47,14 +46,26 @@ class ChatVC: UIViewController {
     func updateWithChannel(){
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
+        getMessages()
     }
     
     func onLogInGetMessages(){
         MessageService.instance.findAllChannels { (success) in
             if success{
-                
+                if MessageService.instance.channels.count > 0{
+                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                    self.updateWithChannel()
+                }else{
+                    self.channelNameLbl.text = "No Channels Yet"
+                }
             }
         }
     }
-
+    
+    func getMessages(){
+        guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+        MessageService.instance.findAllMessagesForChannel(channelId: channelId) { (success) in
+            
+        }
+    }
 }
--- teletubbies/Services/MessageService.swift
+++ teletubbies/Services/MessageService.swift
@@ -17,6 +17,8 @@ class MessageService {
     var channels = [Channel]()
     var selectedChannel: Channel?
     
+    var messages = [Message]()
+    
     func findAllChannels(completion: @escaping CompletionHandler){
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (respones) in
             if respones.result.error == nil {
@@ -41,6 +43,39 @@ class MessageService {
     
     func clearChannels(){
         channels.removeAll()
+    }
+    
+    func findAllMessagesForChannel(channelId: String,completion: @escaping CompletionHandler){
+        
+        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (respones) in
+            if respones.result.error == nil{
+                self.clearMessages()
+                guard let data = respones.data else{return}
+                if let json = JSON(data: data).array{
+                    for item in json {
+                        let messageBody = item["messageBody"].stringValue
+                        let channelId = item["channelId"].stringValue
+                        let id = item["_id"].stringValue
+                        let userName = item["userName"].stringValue
+                        let userAvatar = item["userAvatar"].stringValue
+                        let userAvatarColor = item["userAvatarColor"].stringValue
+                        let timeStamp = item["timeStamp"].stringValue
+                        let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                        self.messages.append(message)
+                        
+                    }
+                    print(self.messages)
+                    completion(true)
+                }
+            }else{
+                debugPrint(respones.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+    func clearMessages(){
+        messages.removeAll()
     }
     
 }
