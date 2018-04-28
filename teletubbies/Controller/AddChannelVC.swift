4f3ac603f75fa27f6c64ca2958654489c2df7cca
Faris Dahleh <farisdahleh@Live.com>
2018-04-28 20:55:12 +0300

    Add Channel VC

---
+++ teletubbies/Controller/AddChannelVC.swift
@@ -0,0 +1,48 @@
+//
+//  AddChannelVC.swift
+//  teletubbies
+//
+//  Created by Faris Dahleh on 4/28/18.
+//  Copyright © 2018 Faris Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    
+    //Outlets
+    
+    @IBOutlet var nameTxt: UITextField!
+    @IBOutlet var descriptionTxt: UITextField!
+    @IBOutlet var bgView: UIView!
+    
+    let placeHolderColor = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    @IBAction func createPressed(_ sender: Any) {
+        
+    }
+    
+    func setupView(){
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "Name", attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+        descriptionTxt.attributedPlaceholder = NSAttributedString(string: "Description", attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+
+    }
+    
+    @objc func closeTap(_ recognaizer: UITapGestureRecognizer){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- teletubbies/Controller/ChannelVC.swift
+++ teletubbies/Controller/ChannelVC.swift
@@ -78,5 +78,10 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     
-
+    @IBAction func addChannelPressed(_ sender: Any) {
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+    }
+    
 }
