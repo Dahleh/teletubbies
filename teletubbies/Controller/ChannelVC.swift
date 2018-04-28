f64b119b8b4bb5d842f3715c5e31e1c182fbc758
Faris Dahleh <farisdahleh@Live.com>
2018-04-28 03:26:26 +0300

    LoggedIn Interface

--- teletubbies/Controller/ChannelVC.swift
+++ teletubbies/Controller/ChannelVC.swift
@@ -13,11 +13,14 @@ class ChannelVC: UIViewController {
     
     @IBOutlet var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
+    @IBOutlet var userImg: RoundedImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
@@ -25,4 +28,15 @@ class ChannelVC: UIViewController {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
+    @objc func userDataDidChanged(_ notif: Notification){
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+            
+        }else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear        }
+    }
 }
